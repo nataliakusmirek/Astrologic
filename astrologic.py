ce1ebb1053aa9d39291938c1e3171c506d8002c3
Natalia Kusmirek <67024662+nataliakusmirek@users.noreply.github.com>
2021-11-23 12:43:53 -0500

    Found a vital error!

--- astrologic.py
+++ astrologic.py
@@ -23,38 +23,38 @@ time.sleep(4)
 # change cancer and everything after it
 # AQUARIUS
 if s == 1 and p >= 20 and p <= 31 or s == 2 and p >= 1 and p <= 18:
-    print("Your results are in, {n}. You are a aquarius!")
+    print(f"Your results are in, {n}. You are a aquarius!")
 # ARIES
 if s == 3 and p >= 21 and p <= 31 or s == 4 and p >= 1 and p <= 19:
-    print("Your results are in, {n}. You are a aries!")
+    print(f"Your results are in, {n}. You are a aries!")
 # CAPRICORN
 if s == 1 and p >= 1 and p <= 19 or s == 12 and p >= 22 and p <= 31:
-    print("Your results are in, {n}. You are a capricorn!")
+    print(f"Your results are in, {n}. You are a capricorn!")
 # CANCER
 if s == 6 and p >= 21 and p <= 30 or s == 7 and p >= 1 and p <= 22:
-    print("Your results are in, {n}. You are a cancer!")
+    print(f"Your results are in, {n}. You are a cancer!")
 # GEMINI
 if s == 5 and p >= 21 and p <= 31 or s == 6 and p >= 1 and p <= 20:
-    print("Your results are in, {n}. You are a gemini!")
+    print(f"Your results are in, {n}. You are a gemini!")
 # LIBRA
 if s == 9 and p >= 23 and p <= 30 or s == 10 and p >= 1 and p <= 22:
-    print("Your results are in, {n}. You are a libra!")
+    print(f"Your results are in, {n}. You are a libra!")
 # LEO
 if s == 7 and p >= 22 and p <= 31 or s == 8 and p >= 1 and p <= 22:
-    print("Your results are in, {n}. You are a leo!")
+    print(f"Your results are in, {n}. You are a leo!")
 # TAURUS
 if s == 4 and p >= 20 and p <= 30 or s == 5 and p >= 1 and p <= 20:
-    print("Your results are in, {n}. You are a taurus!")
+    print(f"Your results are in, {n}. You are a taurus!")
 # VIRGO
 if s == 8 and p >= 23 and p <= 31 or s == 9 and p >= 1 and p <= 22:
-    print("Your results are in, {n}. You are a virgo!")
+    print(f"Your results are in, {n}. You are a virgo!")
 # SCORPIO
 if s == 10 and p >= 23 and p <= 31 or s == 11 and p >= 1 and p <= 21:
-    print("Your results are in, {n}. You are a scorpio!")
+    print(f"Your results are in, {n}. You are a scorpio!")
 # SAGITTARIUS
 if s == 11 and p >= 22 and p <= 30 or s == 12 and p >= 1 and p <= 21:
-    print("Your results are in, {n}. You are a sagittarius!")
+    print(f"Your results are in, {n}. You are a sagittarius!")
 # PISCES
 if s == 2 and p >= 19 and p <= 29 or s == 3 and p >= 1 and p <= 20:
-    print("Your results are in, {n}. You are a pisces!")
+    print(f"Your results are in, {n}. You are a pisces!")
 
